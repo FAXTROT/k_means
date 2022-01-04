bcacd3f1a871808b10a38c9aa475142efd485379
Fakhriddin <ifakhriddin68@gmail.com>
2022-01-04 15:36:02 +0500

    Unused part of the code is deleted

--- main.py
+++ main.py
@@ -2,45 +2,45 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 data = np.array([        # dataset
-    [1, 7],
-    [1.1, 8],
-    [1.3, 7.6],
-    [1.4, 8.1],
-    [1.7, 8.3],
-    [2.5, 7.3],
-    [2.21, 5],
-    [2, 6],
-    [3, 6],
-    [1.2, 3],
-    [2, 7.5],
-    [2, 7],
-    [3, 7.5],
-    [2.6, 6.6],
-    [1.5, 7.3],
-    [1.5, 6.1],
-    [3, 7],
-    [3.5, 1],
-    [4.9, 7.8],
-    [9, 1],
-    [8, 8.8],
-    [6,4],
-    [6.2, 3],
-    [7, 3.5],
-    [7.5, 4],
-    [7.5, 3],
-    [8, 2],
-    [8, 3],
-    [7.6, 2.5],
-    [6, 5],
-    [6.6, 4],
-    [7, 5],
-    [7.4, 5.6],
-    [7.1, 4.9],
-    [6.5, 4.5],
-    [6.9, 1.9],
-    [8, 5],
-    [9, 5.5],
-    [9, 7],
+    [1, 7,],
+    [1.1, 8,],
+    [1.3, 7.6,],
+    [1.4, 8.1,],
+    [1.7, 8.3,],
+    [2.5, 7.3,],
+    [2.21, 5,],
+    [2, 6,],
+    [3, 6,],
+    [1.2, 3,],
+    [2, 7.5,],
+    [2, 7,],
+    [3, 7.5,],
+    [2.6, 6.6,],
+    [1.5, 7.3,],
+    [1.5, 6.1,],
+    [3, 7,],
+    [3.5, 1,],
+    [4.9, 7.8,],
+    [9, 1,],
+    [8, 8.8,],
+    [6,4,],
+    [6.2, 3,],
+    [7, 3.5,],
+    [7.5, 4,],
+    [7.5, 3,],
+    [8, 2,],
+    [8, 3,],
+    [7.6, 2.5,],
+    [6, 5,],
+    [6.6, 4,],
+    [7, 5,],
+    [7.4, 5.6,],
+    [7.1, 4.9,],
+    [6.5, 4.5,],
+    [6.9, 1.9,],
+    [8, 5,],
+    [9, 5.5,],
+    [9, 7,],
     ])
 
 x = data[:, 0]
@@ -50,8 +50,8 @@ plt.plot(x, y, 'rx')
 plt.title("Initial data")
 plt.show()
 
-mug = np.random.random([2, 1]) * 10
-muy = np.random.random([2, 1]) * 10   # centroids
+mug = np.random.random([2, 1])*10
+muy = np.random.random([2, 1])*10   # centroids
 
 plt.plot(x, y, 'rx')
 plt.plot(mug[0], mug[1], 'g.')
@@ -75,13 +75,6 @@ for i in range(0, epochs):
 
     cluster_g = np.delete(cluster_g, dg > dy, axis=0 )    # delete from dataset points which are more close to another centroid
     cluster_y = np.delete(cluster_y, dg < dy, axis=0 )
-    """
-    for key, val in enumerate(idx):
-        if val:
-            cluster_y = np.delete(cluster_y, data[key], axis=0 )
-        else:
-            cluster_g = np.delete(cluster_g, data[key], axis=0 )
-    """
 
     x_g = cluster_g[:, 0]
     y_g = cluster_g[:, 1]
